180efe431780182494b957b97672a6244306908e
Atullya <100409703+atullya@users.noreply.github.com>
2025-03-16 12:03:21 +0545

    Update MidTermExam.aspx.cs

--- MidTermPractise/MidTermPractise/MidTermExam.aspx.cs
+++ MidTermPractise/MidTermPractise/MidTermExam.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,10 +15,11 @@ namespace MidTermPractise
         {
             if (!IsPostBack)
             {
+                CreateTableIfNotExists(); // Ensure table exists
                 LoadData();  // Load data when the page loads initially
             }
-
         }
+
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
             try
@@ -37,48 +38,81 @@ namespace MidTermPractise
                 {
                     faculty = coun.SelectedValue;
                 }
-                //database connection
+
+                // Database connection
                 string cs = "Data Source=DESKTOP-AHSLSJ6;Initial Catalog=db_nccsb;Integrated Security=true";
-                SqlConnection conn = new SqlConnection(cs);
-                conn.Open();
-                string query = "INSERT INTO tbl_reg1  (UserID, Username, Password, Gender, Faculty, Country) VALUES (@UserID, @Username, @Password, @Gender, @Faculty, @Country)";
-                SqlCommand cmd = new SqlCommand(query, conn);
-                cmd.Parameters.AddWithValue("@UserID", id);
-                cmd.Parameters.AddWithValue("@Username", username);
-                cmd.Parameters.AddWithValue("@Password", password);
-                cmd.Parameters.AddWithValue("@Gender", gender);
-                cmd.Parameters.AddWithValue("@Faculty", faculty);
-                cmd.Parameters.AddWithValue("@Country", country);
-                int res = cmd.ExecuteNonQuery();
-                if (res > 0)
+                using (SqlConnection conn = new SqlConnection(cs))
                 {
-                    lblMessage.Text = "Data Inserted";
-                    LoadData();
+                    conn.Open();
+                    CreateTableIfNotExists(); // Ensure table exists before inserting data
+
+                    string query = "INSERT INTO tbl_reg1  (UserID, Username, Password, Gender, Faculty, Country) VALUES (@UserID, @Username, @Password, @Gender, @Faculty, @Country)";
+                    using (SqlCommand cmd = new SqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@UserID", id);
+                        cmd.Parameters.AddWithValue("@Username", username);
+                        cmd.Parameters.AddWithValue("@Password", password);
+                        cmd.Parameters.AddWithValue("@Gender", gender);
+                        cmd.Parameters.AddWithValue("@Faculty", faculty);
+                        cmd.Parameters.AddWithValue("@Country", country);
+
+                        int res = cmd.ExecuteNonQuery();
+                        if (res > 0)
+                        {
+                            lblMessage.Text = "Data Inserted Successfully!";
+                            LoadData();
+                        }
+                        else
+                        {
+                            lblMessage.Text = "Data not Inserted.";
+                        }
+                    }
                 }
-                else
-                {
-                    lblMessage.Text = "Data not Inserted";
-                }
-                conn.Close();
-              
             }
             catch (SqlException s)
             {
-                Console.WriteLine(s);
+                lblMessage.Text = "Database Error: " + s.Message;
             }
-       
         }
+
         public void LoadData()
         {
             string cs = "Data Source=DESKTOP-AHSLSJ6;Initial Catalog=db_nccsb;Integrated Security=true";
             SqlConnection conn = new SqlConnection(cs);
-            conn.Open();
-            string query = "SELECT UserID, Username, Gender, Faculty, Country FROM tbl_reg1";
-            SqlDataAdapter da = new SqlDataAdapter(query, conn);
-            DataTable dt = new DataTable();
-            da.Fill(dt);
-            gridView.DataSource = dt;
-            gridView.DataBind();
+            
+                conn.Open();
+                string query = "SELECT UserID, Username, Gender, Faculty, Country FROM tbl_reg1";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                gridView.DataSource = reader;
+                gridView.DataBind();
+            
+        }
+
+        private void CreateTableIfNotExists()
+        {
+            string cs = "Data Source=DESKTOP-AHSLSJ6;Initial Catalog=db_nccsb;Integrated Security=true";
+            using (SqlConnection conn = new SqlConnection(cs))
+            {
+                conn.Open();
+                string query = @"
+                    IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'tbl_reg1')
+                    BEGIN
+                        CREATE TABLE tbl_reg1 (
+                            UserID NVARCHAR(50) PRIMARY KEY,
+                            Username NVARCHAR(100) NOT NULL,
+                            Password NVARCHAR(100) NOT NULL,
+                            Gender NVARCHAR(10) NOT NULL,
+                            Faculty NVARCHAR(100) NOT NULL,
+                            Country NVARCHAR(255) NOT NULL
+                        )
+                    END";
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
